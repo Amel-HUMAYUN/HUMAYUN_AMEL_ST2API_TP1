e74d9e070e6eacbb8c682429e07ab9fe6d4d2d97
Amel-HUMAYUN <84172451+Amel-HUMAYUN@users.noreply.github.com>
2022-05-23 16:26:04 +0200

    Exo 2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+using System;
 
 namespace Exo_2
 {
     class Program
     {
+        static void Prime()
+        {
+          
+            for (int num = 1; num <= 1000; num++)
+            {
+                int ctr = 0;
+
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+
+                if (ctr == 0 && num != 1)
+                    Console.Write("{0} ", num);
+            }
+            Console.Write("\n");
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Prime();
         }
     }
 }
