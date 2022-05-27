dd1b40e4d9e1458d72b1adf2907fad2b954a5f45
Amel-HUMAYUN <84172451+Amel-HUMAYUN@users.noreply.github.com>
2022-05-27 10:31:26 +0200

    Exo1.cs

--- HUMAYUN_AMEL_ST2API_TP1/HUMAYUN_AMEL_ST2API_TP1/Exo1.cs
+++ HUMAYUN_AMEL_ST2API_TP1/HUMAYUN_AMEL_ST2API_TP1/Exo1.cs
@@ -4,7 +4,7 @@ namespace HUMAYUN_AMEL_ST2API_TP1
 {
     public class Exo1
     {
-
+        # table of multiplication 
         public void Multiplication()
         {
             for (int i = 1; i <= 10; i++)
@@ -15,12 +15,12 @@ namespace HUMAYUN_AMEL_ST2API_TP1
                         Console.Write("{0}x{1} = {2}, ", j, i, i * j);
                     else
                         Console.Write("{0}x{1} = {2}", i, j, i * j);
-
                 }
                 Console.Write("\n");
             }
         }
-
+        
+        # table of multiplication and it will only print odd number
         public void MultiplicationOdd()
         {
             for (int i = 1; i< 11; i++)
@@ -34,27 +34,23 @@ namespace HUMAYUN_AMEL_ST2API_TP1
                     {
                         Console.WriteLine("{0}x{1} = {2}", i, j,mul);
                     }
-
                 }
             }
-            
         }
 
+        # table of multiplication and ask a number to user
         public void MultiplicationaskNumber(int n)
         {
-            
                 for (int j = 1; j <= 10; j++)
                 {
                     if (j <= 10 - 1)
                         Console.Write("{0}x{1} = {2}, ", j, n, n * j);
                     else
                         Console.Write("{0}x{1} = {2}", n, j, n * j);
-
                 }
                 Console.Write("\n");
             
         }
-
     }
 }
 
